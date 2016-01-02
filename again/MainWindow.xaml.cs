6e4b993aff5136ef6a7ab4535004ed00c0396cbe
zhaozhiqiang <iezhaozhiqiang@gmail.com>
2016-01-03 00:54:32 +0800

    add main windows
    have not provide a useable version.

---
+++ again/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+
+namespace again
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string MusicDir = "";
+
+        private TimeSpan Position = new TimeSpan(0, 0, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            play();
+        }
+
+        private bool isMusicExist()
+        {
+            if (File.Exists(MusicDir))
+            {
+                player.Source = new Uri(MusicDir);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void play()
+        {
+            if (isMusicExist())
+            {
+                try
+                {
+                    player.Position = Position;
+                    player.Play();
+                }
+                catch (NotSupportedException)
+                {
+                    //do nothing.
+                }
+            }
+        }
+    }
+}
